767ef11af0a42286e138f8c6646c416f71125be7
Toxiquee <hakim92120@gmail.com>
2024-01-25 18:50:53 +0100

    test

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View} from "react-native";
+import Routes from "./routes";
+const App = () =>{
+    return(
+        <View>
+            <Routes />
+        </View>
+    );
+};
+
+export default App;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Routes = () => {
+  return (
+    <View>
+      <Text>Test YES</Text>
+    </View>
+  )
+}
+
+export default Routes
